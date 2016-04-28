4033592f770ad5c7c16e96a89025dabba12b4393
Evgeny Shpilevsky <evgeny@shpilevsky.com>
2016-04-29 00:45:17 +0300

    Optimize variables

--- inject.js
+++ inject.js
@@ -10,10 +10,22 @@ var empty = function () {
     };
 };
 
+var booleanNode = function (value) {
+    return {
+        type: 'BooleanLiteral',
+        value: value,
+        original: value
+    };
+};
+
 injectify.installPlugin(function (injectify) {
     var blocks = injectify.options.condition || {};
 
     var accept = function (node) {
+        if (!node.path) {
+            return false;
+        }
+
         return node.path.original in blocks;
     };
 
@@ -24,6 +36,10 @@ injectify.installPlugin(function (injectify) {
             return source.inverse || empty();
         }
     };
+
+    // injectify.bus.on('node', function(event) {
+    //     console.log(JSON.stringify(event.node, null, 2));
+    // })
 
     injectify.walker.registerTransform('BlockStatement', function (node) {
         var i, param;
@@ -50,6 +66,22 @@ injectify.installPlugin(function (injectify) {
                     return modify(param, node, false);
                 }
             }
+        }
+
+        return node;
+    });
+
+    // injectify.walker.registerTransform('HashPair', function (node) {
+    //     if (node.value.type === 'SubExpression' && accept(node.value)) {
+    //         node.value = booleanNode(blocks[node.value.path.original]);
+    //     }
+    //
+    //     return node;
+    // });
+    //
+    injectify.walker.registerTransform('SubExpression', function (node) {
+        if (accept(node)) {
+            return booleanNode(blocks[node.path.original]);
         }
 
         return node;
--- spec/index.js
+++ spec/index.js
@@ -1,41 +1,68 @@
 var expect = require('chai').expect;
+var Handlebars = require('injectify/runtime');
+
+Handlebars.registerHelper('block-helper', function (options) {
+    return options.fn(options.hash);
+});
+
+Handlebars.registerHelper('value-helper', function (value1, value2, options) {
+    return options.fn({value1: value1, value2: value2});
+});
 
 describe('injectify-condition', function () {
 
-    // it('block helper', function() {
-    //     var blockTemplate = require('./fixture/block-helper.hbs');
-    //
-    //     expect(blockTemplate({}).trim()).to.equal('mobile then');
-    // });
-    //
-    // it('block helper without else block', function() {
-    //     var blockTemplate = require('./fixture/block-helper-without-else.hbs');
-    //
-    //     expect(blockTemplate({}).trim()).to.equal('');
-    // });
+    it('block helper', function () {
+        var blockTemplate = require('./fixture/block-helper.hbs');
 
-    it('if block helper', function() {
+        expect(blockTemplate({}).trim()).to.equal('mobile then');
+    });
+
+    it('block helper without else block', function () {
+        var blockTemplate = require('./fixture/block-helper-without-else.hbs');
+
+        expect(blockTemplate({}).trim()).to.equal('');
+    });
+
+    it('if block helper', function () {
         var blockTemplate = require('./fixture/if-block-helper.hbs');
 
         expect(blockTemplate({}).trim()).to.equal('then');
     });
 
-    it('if block helper without else', function() {
+    it('if block helper without else', function () {
         var blockTemplate = require('./fixture/if-block-helper-without-else.hbs');
 
         expect(blockTemplate({}).trim()).to.equal('');
     });
 
-    it('unless block helper', function() {
+    it('unless block helper', function () {
         var blockTemplate = require('./fixture/unless-block-helper.hbs');
 
         expect(blockTemplate({}).trim()).to.equal('then');
     });
 
-    it('unless block helper without else', function() {
+    it('unless block helper without else', function () {
         var blockTemplate = require('./fixture/unless-block-helper-without-else.hbs');
 
         expect(blockTemplate({}).trim()).to.equal('');
+    });
+
+    it('unless block helper without else', function () {
+        var blockTemplate = require('./fixture/skip-if.hbs');
+
+        expect(blockTemplate({}).trim()).to.equal('else');
+    });
+
+    it('pass as hash to block helper', function () {
+        var blockTemplate = require('./fixture/pass-as-hash-to-block.hbs');
+
+        expect(blockTemplate({}).trim()).to.equal('value - true - false');
+    });
+
+    it('pass as value to block helper', function () {
+        var blockTemplate = require('./fixture/pass-as-value-to-block.hbs');
+
+        expect(blockTemplate({}).trim()).to.equal('value - true - false');
     });
 
 });
